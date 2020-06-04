dba7797947da370e16443880eab0552bf39e3185
Marcos Sousa <marcos.sousa@outlook.com>
2020-06-04 00:01:15 -0300

    Criação do server em andamento.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,10 @@ import express from 'express';
 
 const app = express();
 
-const users = ['Luiz', 'Rita', 'Milena', 'Marcos', 'Miller',;
+const users = ['Luiz', 'Rita', 'Milena', 'Marcos', 'Miller'];
 
-app.get('/', (request, response)=>{
-  console.log('Funcionou!');
-  return response.json();
+app.get('/users', (request, response)=>{
+  return response.json(users);
 });
 
 app.listen(3333);
